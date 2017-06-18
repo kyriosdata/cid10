464c892ad1e9c6091ef97dac150a40bfdb74abab
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2017-06-18 17:31:56 -0300

    POST parâmetro do corpo "q=t1 t2"

--- ms/README.md
+++ ms/README.md
@@ -1,10 +1,11 @@
-# Microserviço
-Implementação de _endpoint_ para busca em códigos da CID-10.
+ Microserviço
+Implementação de microserviço que responde requisições de consulta por
+entradas da CID-10.
 
 # Uso
 Via linha de comandos uma requisição pode ser disparada conforme abaixo,
 onde a entrada da CID-10 procurada deve incluir **engue** assim como **90**, seja na descrição ou no código.
 
 ```
-curl -G "http://localhost:8080/cid" --data-urlencode "q=engue 90"
+curl -X POST http://localhost:8080/cid -d "q=engue 90"
 ```
--- ms/src/main/java/com/github/kyriosdata/bsus/Aplicacao.java
+++ ms/src/main/java/com/github/kyriosdata/bsus/Aplicacao.java
@@ -2,17 +2,15 @@ package com.github.kyriosdata.bsus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class Aplicacao {
 
-	@RequestMapping(value = "/cid", params = { "q" })
-	public String search(@RequestParam("q") String q) {
-		return q + ";outra entrada";
+	@RequestMapping(value = "/cid", method = RequestMethod.POST)
+	public String search(@RequestParam String q) {
+		return "RECEBIDO: " + q;
 	}
 
 	public static void main(String[] args) {
