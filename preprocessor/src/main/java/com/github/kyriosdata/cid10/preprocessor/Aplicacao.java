a33c01408a4ceaaebaca222e0b98502bd7eb6635
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-12-10 16:59:20 -0200

    org

--- preprocessor/src/main/java/com/github/kyriosdata/cid10/preprocessor/Aplicacao.java
+++ preprocessor/src/main/java/com/github/kyriosdata/cid10/preprocessor/Aplicacao.java
@@ -12,14 +12,15 @@
 
 package com.github.kyriosdata.cid10.preprocessor;
 
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class Aplicacao {
     public static void main(String[] args) throws Exception {
-        Path grupos = FileFromResourcesFolder.getPath("databus/CID-10-grupos.CSV");
-        List<String> linhas = Files.readAllLines(grupos);
+        Path grupos = FileFromResourcesFolder.getPath("datasus/CID-10-GRUPOS.CSV");
+        List<String> linhas = Files.readAllLines(grupos, Charset.forName("ISO-8859-1"));
         linhas.forEach(l -> System.out.println(l));
     }
 }
