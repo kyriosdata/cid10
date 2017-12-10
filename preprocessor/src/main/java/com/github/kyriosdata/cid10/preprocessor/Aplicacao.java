c36ae5b303e5be00c4e3a06816517c1d12a6cee7
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-12-10 16:30:15 -0200

    org

---
+++ preprocessor/src/main/java/com/github/kyriosdata/cid10/preprocessor/Aplicacao.java
@@ -0,0 +1,19 @@
+/*
+ *
+ *  Copyright (c) 2017
+ *
+ *  Fábio Nogueira de Lucena
+ *  Fábrica de Software - Instituto de Informática (UFG)
+ *
+ *  Creative Commons Attribution 4.0 International License.
+ *
+ *
+ */
+
+package com.github.kyriosdata.cid10.preprocessor;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
