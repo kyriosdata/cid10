2fc9ba6017dc5a6fcf2734909b63901964b29223
kyriosdata <fabio@inf.ufg.br>
2017-12-14 19:10:24 -0200

    montagem da API

---
+++ cid-busca/src/main/java/com/github/kyriosdata/cid10/servico/modelo/Grupo.java
@@ -0,0 +1,10 @@
+package com.github.kyriosdata.cid10.servico.modelo;
+
+/**
+ * Um grupo da CID-10.
+ */
+public class Grupo {
+    public String ci;
+    public String cf;
+    public String descricao;
+}
