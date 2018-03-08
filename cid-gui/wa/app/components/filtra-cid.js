da6c172be5412a26930a0d05137e2dcffde8f54a
kyriosdata <fabio@inf.ufg.br>
2018-03-08 17:27:15 -0300

    filtro

--- cid-gui/wa/app/components/filtra-cid.js
+++ cid-gui/wa/app/components/filtra-cid.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames : [ "filtra-cid" ],
+  value: '',
+
+  init() {
+    this._super(...arguments);
+    this.get('filter')('').then((results) => this.set('results', results));
+  },
+
+  actions : {
+    handleEntradaFiltro() {
+      let filterInputValue = this.get("value");
+      let filterAction = this.get("filter");
+      filterAction(filterInputValue).then((filterResults) => this.set("results", filterResults));
+    }
+  }
 });
