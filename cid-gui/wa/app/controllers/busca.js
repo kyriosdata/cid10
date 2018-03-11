7cb1ab7210b83105678ddfc3e35f59c9eb83fb3a
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-03-11 09:46:16 -0300

    search

--- cid-gui/wa/app/controllers/busca.js
+++ cid-gui/wa/app/controllers/busca.js
@@ -1,17 +1,23 @@
 import Controller from '@ember/controller';
 import $ from "jquery";
 
+const HOST = "http://localhost:8080/busca/";
+
+function montaURL(argumentos, ordem) {
+  return HOST + argumentos + "/" + ordem;
+}
+
 export default Controller.extend({
   actions: {
     filtraPorParte(param) {
       if (param != "") {
         // return this.get("store").query("entrada", { city: param })
         const tratado = param.toString().trim();
-        const url = "http://localhost:8080/busca/" + param + "/0";
+        const url = montaURL(param, 0);
         console.log(url);
         return $.getJSON(url);
       } else {
-        return $.getJSON("http://localhost:8080/busca/a923/0");
+        return $.getJSON(montaURL("a923", 0));
       }
     }
   }
