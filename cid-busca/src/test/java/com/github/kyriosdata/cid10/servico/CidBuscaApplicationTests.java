518fdf195552fe123688980f14016c111b1b2140
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-07-06 19:33:12 -0300

    upgrade to junit5

--- cid-busca/src/test/java/com/github/kyriosdata/cid10/servico/CidBuscaApplicationTests.java
+++ cid-busca/src/test/java/com/github/kyriosdata/cid10/servico/CidBuscaApplicationTests.java
@@ -1,12 +1,12 @@
 package com.github.kyriosdata.cid10.servico;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -17,7 +17,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CidBuscaApplicationTests {
