ef99323ee18d3ced669b820e120c07a51ef97ee5
kyriosdata <fabio@inf.ufg.br>
2022-09-15 14:43:52 -0300

    remove ember preferencia para react

--- sandbox/cid-gui/wa/app/helpers/exibe-entrada-cid.js
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { htmlSafe } from '@ember/string';
-
-export function exibeEntradaCid(entrada) {
-  const campos = entrada.toString().split(";");
-  return htmlSafe(`<tr><td>${campos[0]}</td><td>${campos[2]}</td></tr>`);
-}
-
-export default helper(exibeEntradaCid);
--- sandbox/cid-gui/wa/app/routes/busca.js
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  model() {
-    return ["códigoA;-;descriçãoA", "códigoB;-;descriçãoB"];
-  }
-});
